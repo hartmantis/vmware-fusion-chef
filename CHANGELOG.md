8020934dc5a8373581b54b0e6950ff16f8d5ec5d
Jonathan Hartman <j@p4nt5.com>
2015-07-28 23:09:11 -0700

    Bump version for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 VMware Fusion Cookbook CHANGELOG
 ================================
 
+v0.1.0 (2015-07-28)
+-------------------
+- Initial release!
+
 v0.0.1 (2015-07-09)
 -------------------
 - Development started
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'j@p4nt5.com'
 license          'apache2'
 description      'Installs VMWare Fusion'
 long_description 'Installs VMWare Fusion'
-version          '0.0.1'
+version          '0.1.0'
 
 depends          'dmg', '~> 2.2'
 
