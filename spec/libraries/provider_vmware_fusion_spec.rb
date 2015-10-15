2fb3ef5eb5d9b6ce3830ad5874ca6ee82e422630
Jonathan Hartman <j@p4nt5.com>
2015-10-14 21:23:15 -0700

    Update unit tests for Chef 12.5

--- spec/libraries/provider_vmware_fusion_spec.rb
+++ spec/libraries/provider_vmware_fusion_spec.rb
@@ -5,8 +5,9 @@ require_relative '../../libraries/provider_vmware_fusion'
 
 describe Chef::Provider::VmwareFusion do
   let(:name) { 'default' }
-  let(:new_resource) { Chef::Resource::VmwareFusion.new(name, nil) }
-  let(:provider) { described_class.new(new_resource, nil) }
+  let(:run_context) { ChefSpec::SoloRunner.new.converge.run_context }
+  let(:new_resource) { Chef::Resource::VmwareFusion.new(name, run_context) }
+  let(:provider) { described_class.new(new_resource, run_context) }
 
   describe 'PATH' do
     it 'returns the app directory' do
