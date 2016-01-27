6cccf87230bef60b2c3e2399bca0813b66ac86d4
Jonathan Hartman <j@p4nt5.com>
2016-01-26 22:50:06 -0800

    Update year to 2016

--- libraries/resource_vmware_fusion_app.rb
+++ libraries/resource_vmware_fusion_app.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: vmware_fusion
 # Library:: resource_vmware_fusion_app
 #
-# Copyright 2015 Jonathan Hartman
+# Copyright 2015-2016, Jonathan Hartman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
